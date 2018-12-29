0c35b40952f5553fed2ece998e3f5cbabcad0701
stome <143688@126.com>
2018-12-29 18:04:53 +0800

    frist

---
+++ tornad/project/setting.py
@@ -0,0 +1,9 @@
+import os
+
+
+settings = {
+    "static_path": os.path.join(os.path.dirname(__file__), "static"),
+    "cookie_secret": "61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
+    "login_url": "/login",
+    "xsrf_cookies": True,
+}
