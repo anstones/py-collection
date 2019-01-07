4d5ac44122f34c6ece503f4efe13f1e41b918d0c
stone <1436882538@qq.com>
2019-01-07 21:00:37 +0800

    django目录优化

--- Mydjango/HelloWorld/HelloWorld/config/settings.py
+++ Mydjango/HelloWorld/HelloWorld/config/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -37,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app1',
-    'snippets',
+    'app1.apps.App1Config',
+    'snippets.apps.SnippetsConfig',
     'rest_framework',
 ]
 
