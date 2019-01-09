09d2c69e73a94e6edcacb71c4f7fdae9a8134d36
stome <143688@126.com>
2019-01-09 10:07:26 +0800

    UP

--- Mydjango/HelloWorld/manage.py
+++ Mydjango/HelloWorld/manage.py
@@ -3,11 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-<<<<<<< HEAD
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HelloWorld.settings.dev')
-=======
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HelloWorld.config.settings')
->>>>>>> 4d5ac44122f34c6ece503f4efe13f1e41b918d0c
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
