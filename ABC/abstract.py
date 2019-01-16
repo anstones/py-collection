80a58d541c14f7916516e3136c657e5a170a0627
stome <143688@126.com>
2019-01-16 18:43:38 +0800

    ==

--- ABC/abstract.py
+++ ABC/abstract.py
@@ -87,8 +87,10 @@ ann = Customer('Ann Smith', 1100)
 cart = [LineItem('banana', 4, .5), 
         LineItem('apple', 10, 1.5), 
         LineItem('watermellon', 5, 5.0)]
+        
 print(Order(joe, cart, FidelityPromo()))
 print(Order(ann, cart, FidelityPromo()))
+
 banana_cart = [LineItem('banana', 30, .5), 
                 LineItem('apple', 10, 1.5)]
 print(Order(joe, banana_cart, BulkItemPromo()))
