06ce2ff28c7ff0f5a3a5272f3f342a6c75d9cdc0
shidebin <shidebin@0easy.com>
2019-03-08 15:39:09 +0800

    add code

--- Py-cookbook/文件与IO.py
+++ Py-cookbook/文件与IO.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+import os
+
 
 # 向一个文件中写入数据，但是前提必须是这个文件在文件系统上不存在
 
 # with open('somefile', 'wt') as f:
-with open('somefile', 'xt') as f:
-    f.write('Hello\n')
+# with open('somefile', 'xt') as f:
+#     f.write('Hello\n')
+
+# 获取某个目录中的文件列表：
+# names = os.listdir('somedir')
+
+
+
+from collections import namedtuple
+import csv
+
+with open('data.csv') as f:
+    f_csv = csv.reader(f)
+    headings = next(f_csv)
+    Row = namedtuple('Row', headings)
+    for r in f_csv:
+        row = Row(*r)
+        print(row.Symbol)
+
+with open('data.csv') as f:
+    f_csv = csv.DictReader(f)
+    for row in f_csv:
+        print(row["Symbol"])
